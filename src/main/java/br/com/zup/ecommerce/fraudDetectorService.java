725babc773f9c36a360f9f423efc540b12f9c18c
Thalyta-dev <thalytamaelycorrea1718@gmail.com>
2021-06-10 18:27:26 -0300

    Refatorando

--- src/main/java/br/com/zup/ecommerce/fraudDetectorService.java
+++ src/main/java/br/com/zup/ecommerce/fraudDetectorService.java
@@ -1,53 +1,32 @@
-package br.com.zup.ecommerce;
+package br.com.alura.ecommerce;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
+public class FraudDetectorService {
 
-public class fraudDetectorService {
-
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
-
-
-        while (true) {
-            var records = consumer.poll(Duration.ofMillis(100));
-
-            for (var record : records) {
-                System.out.println("----------------------------------------------");
-                System.out.println("Processando a nova ordem, checando se é fraude");
-                System.out.println(record.key());
-                System.out.println(record.value());
-                System.out.println(record.partition());
-                System.out.println(record.offset());
-                Thread.sleep(5);
-
-            }
+    public static void main(String[] args) {
+        var fraudService = new FraudDetectorService();
+        try (var service = new KafkaService(FraudDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                fraudService::parse)) {
+            service.run();
         }
     }
 
-
-    private static Properties properties() {
-
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, fraudDetectorService.class.getSimpleName());
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
-        return properties;
-
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("------------------------------------------");
+        System.out.println("Processing new order, checking for fraud");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // ignoring
+            e.printStackTrace();
+        }
+        System.out.println("Order processed");
     }
-}
 
+}
